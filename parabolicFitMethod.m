18c77362907f53642ccb9c0156a1a779434654a6
Francesco Marcato <49764729+marcatofrancesco@users.noreply.github.com>
2021-05-11 12:23:19 +0200

    aggiornato metodo di controllo
    ora la funzione dice che un immagine non è una faccia se il vertice della parabola non è all'interno dell'immagine

--- parabolicFitMethod.m
+++ parabolicFitMethod.m
@@ -3,15 +3,15 @@
 % falseNegative sono immagini ritenute non volti dal fit ma che in realtà sono
 % volti
 
-
 noFace = [];
 counter =1;
 falseNegative = [];
 gg = 1;
 % ho fatto solo le prime 1000 immagini per comodità
+%controllo le prime 1000 immagini dal label 2 (solo faccia)
 for i = 1:1000
     
-    matrix = DepthDATA{i}{3};
+    matrix = DepthDATA{i}{2};
     maxMatrix = max(matrix(:));
     testMedia = matriceMedia(matrix, maxMatrix);
     matrixCenter = round(size(testMedia, 1)/2);
@@ -23,7 +23,9 @@ for i = 1:1000
     f=fit(x,y,'poly2');
     %plot(f,x,y) 
     coefficientValues = coeffvalues(f);
-    if coefficientValues(2)>0.1
+    %if coefficientValues(2)>0.1     vecchio metodo (buono ma senza senso)
+    vertice = -coefficientValues(2)/(2*coefficientValues(1));  %vertice della parabola
+    if (vertice<0 || vertice>size(testMedia, 1)) % controllo che il vertice sia interno all'immagine
         noFace(counter) = i;
         counter = counter +1;
         if DepthDATA{i}{8} == 0.5
@@ -31,6 +33,5 @@ for i = 1:1000
             gg = gg+1;
         end
     end
-
 end
 
