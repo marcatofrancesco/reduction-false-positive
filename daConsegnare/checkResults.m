9f440914af38a3417cea773cda15309262800d20
Francesco Marcato <49764729+marcatofrancesco@users.noreply.github.com>
2021-08-02 13:02:02 +0200

    Create checkResults.m
    script che stampa i risultati del metodo

---
+++ daConsegnare/checkResults.m
@@ -0,0 +1,28 @@
+% stampa ad output i rusultati del cluster
+giuste = 0;
+sbagliate = 0;
+nonIndividuate = 0;
+totaleNF = 0;
+for i = 1:size(results)
+    if DepthDATA{i}{8} == 1
+        totaleNF = totaleNF +1;
+    end
+    if DepthDATA{i}{8} == 1 && results(i)== 0.5
+        nonIndividuate = nonIndividuate+1;
+    end
+    if results(i)== 1
+        if DepthDATA{i}{8} == 0.5
+            sbagliate = sbagliate+1;
+        else
+            giuste = giuste+1;
+        end
+    end
+end
+disp("NonFace correttamente individuate: ")
+disp(giuste)
+disp("NonFace erratamente individuate: ")
+disp(sbagliate)
+disp("NonFace non individuate: ")
+disp(nonIndividuate)
+disp("Totale NonFace: ")
+disp(totaleNF)
