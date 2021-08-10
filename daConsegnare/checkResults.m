8bb63a36a823f575ab82adf941a59626f6ef024b
Francesco Marcato <49764729+marcatofrancesco@users.noreply.github.com>
2021-08-10 15:51:30 +0200

    Update checkResults.m

--- daConsegnare/checkResults.m
+++ daConsegnare/checkResults.m
@@ -1,20 +1,21 @@
-% stampa ad output i risultati del cluster
-giuste = 0;
-sbagliate = 0;
-nonIndividuate = 0;
-totaleNF = 0;
-for i = 1:size(results)
+% stampa ad output i risultati ottenuti nell'array results
+% dichiaro contatori
+giuste = 0; % truePositive
+sbagliate = 0; % falsePositive
+nonIndividuate = 0; % FalseNegative
+totaleNF = 0; % totale NonFace
+for i = 1:size(results) % per ogni immagine in result
     if DepthDATA{i}{8} == 1
-        totaleNF = totaleNF +1;
+        totaleNF = totaleNF +1; % aggiorno contatore nonFace
     end
     if DepthDATA{i}{8} == 1 && results(i)== 0.5
-        nonIndividuate = nonIndividuate+1;
+        nonIndividuate = nonIndividuate+1; % aggiorno contatore FalseNegative
     end
     if results(i)== 1
         if DepthDATA{i}{8} == 0.5
-            sbagliate = sbagliate+1;
+            sbagliate = sbagliate+1; % aggiorno contatore falsePositive
         else
-            giuste = giuste+1;
+            giuste = giuste+1; % aggiorno contatore truePositive
         end
     end
 end
